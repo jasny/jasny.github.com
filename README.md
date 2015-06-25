4d5245d6850640dca74669b60c48c8075010a1a7
Arnold Daniels <arnold@jasny.net>
2015-06-25 14:30:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# About the jasny.net website
+
+Feel free to use any part of this website, as it's released under the [CC0](https://creativecommons.org/publicdomain/zero/1.0/legalcode) "No rights reserved".
+
+[![CC0](https://licensebuttons.net/p/zero/1.0/88x31.png)](https://creativecommons.org/publicdomain/zero/1.0/legalcode)
+
+jasny.net is a static website, hosted on [GitHub pages](https://pages.github.com/). It has no installation or build process.
+
+## HTML
+
+The website is written for HTML5 and crafted by hand with the use of a simple text editor.
+
+## CSS
+
+No CSS preprocessors are used, it's just plain old CSS.
+
+### Responsive design
+
+[Media queries](https://developer.mozilla.org/en-US/docs/Web/CSS/@media) and [viewport percentage lengths](https://developer.mozilla.org/en-US/docs/Web/CSS/length#Viewport-percentage_lengths) are used to create a responsive design. The layout transitions seamlessly from one to the other viewports by combining relative units `%`, `vh` and `vw` with the absolute unit `px`. This contrasts most designs, which have specific breakpoints where the layout changes.
+
+Example: An element should have a `10px` padding for a viewport width of `400px` or less. For a width of `900px` or more, the padding should be `20px`. Between `400px` and `900px` we smoothly transition from `10px` to `20px`.
+
+    padding: 10px;
+    
+    @media (min-width: 400px) {
+      padding: calc(2vw + 2px);
+    }
+    
+    @media (min-width: 900px) {
+      padding: 20px;
+    }
+
+## JavaScript
+
+The website uses [vanilla JavaScript](http://vanilla-js.com/) rather that a JS framework. If you're used to working with jQuery and are interrested in writing native JavaScript, check out the website ['You might not need jQuery'](http://youmightnotneedjquery.com).
+
+
+## Browser support
+
+The website is only properly displayed with a browser that support the `transform` property.
+
+Older browsers (like IE9) are displayed a message and link to [outdatedbrowser.com](http://outdatedbrowser.com/). This is done with the outdatedbrowser.js library. AJAX for this library has been disabled.
+
+
---
+++ script.js
@@ -0,0 +1,152 @@
+/*
+ * Feel free to use this code :D (http://www.jasny.net/mit)
+ * Also http://youmightnotneedjquery.com
+ */
+
+
+/**
+ * Show or hide content based on hash change
+ */
+function showHideContent(url) {
+  var wait = true;
+
+  // Hide content
+  if (document.getElementById('about').classList.contains('in')) hideAbout();
+  else if (document.getElementById('canvas').classList.contains('open')) closeCanvas('portfolio');
+  else if (document.getElementById('canvas').classList.contains('slid')) unslideCanvas('projects');
+  else wait = false;
+  
+  if (wait) {
+    if (!url.match(/#$/)) setTimeout(function() { showHideContent(url); }, 1000);
+    return;
+  }
+  
+  // Show content
+  if (url.match(/#about$/)) showAbout();
+  if (url.match(/#portfolio$/)) openCanvas('portfolio');
+  if (url.match(/#projects$/)) slideCanvas('projects');
+}
+
+/**
+ * Show about panel
+ */
+function showAbout() {
+  document.getElementById('about').classList.remove('hidden');
+  document.getElementById('canvas').classList.add('shrunk');
+  
+  setTimeout(function() {
+    document.getElementById('about').classList.add('in');
+  }, 50);
+}
+
+/**
+ * Hide about panel
+ */
+function hideAbout() {
+  document.getElementById('canvas').classList.remove('shrunk');
+  document.getElementById('about').classList.remove('in');
+  
+  setTimeout(function() {
+    if (document.getElementById('about').classList.contains('in')) return;
+    document.getElementById('about').classList.add('hidden');
+  }, 1000);
+}
+
+/**
+ * Open the canvas effect
+ *
+ * @param string id  Element id to show
+ */
+function openCanvas(id) {
+  var canvas = document.getElementById('canvas');
+
+  if (canvas.classList.contains('open')) return;
+
+  document.getElementById(id).classList.remove('hidden');
+
+  canvas.classList.add('open');
+  canvas.classList.add('split');
+
+  setTimeout(function() {
+    document.getElementById('siteTop').classList.add('shadow');
+    document.getElementById('siteBottom').classList.add('shadow');
+  }, 40);
+}
+
+/**
+ * Close the canvas effect
+ */
+function closeCanvas(id) {
+  var canvas = document.getElementById('canvas');
+
+  if (!canvas.classList.contains('open')) return;
+
+  canvas.classList.remove('open');
+
+  setTimeout(function() {
+    if (canvas.classList.contains('open')) return;
+    document.getElementById('siteTop').classList.remove('shadow');
+    document.getElementById('siteBottom').classList.remove('shadow');
+  }, 960);
+
+  setTimeout(function() {
+    if (canvas.classList.contains('open')) return;
+    canvas.classList.remove('split');
+    document.getElementById(id).classList.add('hidden');
+  }, 1000);
+}
+
+/**
+ * Slide the canvas effect
+ */
+function slideCanvas(id) {
+  var canvas = document.getElementById('canvas');
+
+  if (canvas.classList.contains('slid')) return;
+  
+  document.getElementById(id).classList.remove('hidden');
+
+  canvas.classList.add('moved'); 
+  canvas.classList.add('slid');
+}
+
+/**
+ * Slide the canvas back effect
+ */
+function unslideCanvas(id) {
+  document.getElementById('canvas').classList.remove('slid');
+  
+  setTimeout(function() {
+    if (canvas.classList.contains('slid')) return;
+    document.getElementById('canvas').classList.remove('moved');
+    document.getElementById(id).classList.add('hidden');
+  }, 1000);
+}
+
+/**
+ * Initialize
+ */
+(function() {
+  // Go back to initial state when clicking on the canvas
+  Array.prototype.forEach.call(document.querySelectorAll('#canvas > *'), function(el) {
+    el.addEventListener('click', function(event) {
+      if (window.location.href.match(/#.+$/) && event.target.tagName !== 'A') window.location = '#';
+    });
+  });
+  
+  // Add handler when URL # changes
+  window.addEventListener('hashchange', function(event) {
+    showHideContent(event.newURL);
+  }, false);
+  
+  window.addEventListener('load', function() {
+    // Show content based on current #
+    if (window.location.href.match(/#.+$/)) setTimeout(function() {
+       showHideContent(window.location.href);
+    }, 500);
+    
+    // Perfect scrollbar
+    Ps.initialize(document.getElementById('about'));
+  });
+})();
+
